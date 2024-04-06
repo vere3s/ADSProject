1cc4e590398931fc8917b7d64e5b6279ecc09463
vereSlam <126106491+vereSlam@users.noreply.github.com>
2024-04-06 11:54:54 -0600

    Se agregan validaciones para el modelo de Grupo

--- ADSProject/Controllers/GruposController.cs
+++ ADSProject/Controllers/GruposController.cs
@@ -24,6 +24,10 @@ namespace ADSProject.Controllers
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
                 int contador = this.grupo.AgregarGrupo(grupo);
                 if (contador > 0)
                 {
@@ -49,6 +53,10 @@ namespace ADSProject.Controllers
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
                 int contador = this.grupo.ActualizarGrupo(idGrupo, grupo);
                 if (contador > 0)
                 {
--- ADSProject/Models/Grupo.cs
+++ ADSProject/Models/Grupo.cs
@@ -1,12 +1,19 @@
-﻿namespace ADSProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADSProject.Models
 {
     public class Grupo
     {
         public int IdGrupo { get; set; }
+        [Required(ErrorMessage = "Este es un campo requerido")]
         public int IdCarrera { get; set; }
+        [Required(ErrorMessage = "Este es un campo requerido")]
         public int IdMateria { get; set; }
+        [Required(ErrorMessage = "Este es un campo requerido")]
         public int IdProfesor { get; set; }
+        [Required(ErrorMessage = "Este es un campo requerido")]
         public int Ciclo { get; set; }
+        [Required(ErrorMessage = "Este es un campo requerido")]
         public int Anio { get; set; }
     }
 }
